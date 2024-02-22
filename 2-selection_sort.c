77a94e1713bd2c31d0e8a522d25dea83033b1ec1
K-kare <kamunawangari@gmail.com>
2024-02-22 04:47:22 -0800

    2-main.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+*selection_sort - func that sort in selection sort
+*@array: array that contains the element
+**/
+void selection_sort(int *array, size_t size)
+{
+int temp;
+size_t i;
+size_t j;
+int min;
+for (i = 0; i < size - 1; i++)
+{
+min = i;
+for (j = i + 1; j < size; j++)
+{
+if (array[min] > array[j])
+{
+min = j;
+}
+}
+temp = array[i];
+array[i] = array[min];
+array[min] = temp;
+print_array(array, size);
+}
+}
